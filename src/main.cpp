4f108614886b693f06331bec51d6572b9b7e7546
SeongYongPark <pji50260@naver.com>
2023-10-16 17:32:20 +0900

    10_16

--- src/main.cpp
+++ src/main.cpp
@@ -30,18 +30,18 @@ int main()
 
     // Motor Declariration
     std::map<std::string, std::shared_ptr<TMotor>> tmotors;
-    tmotors["1_waist"] = std::make_shared<TMotor>(0x07, "AK10_9", "can0");
+    tmotors["1_waist"] = std::make_shared<TMotor>(0x07, "AK10_9", "can1");
     
-    tmotors["2_R_arm1"] = std::make_shared<TMotor>(0x02, "AK70_10", "can2");
-    tmotors["3_L_arm1"] = std::make_shared<TMotor>(0x04, "AK70_10", "can2");
-    tmotors["4_R_arm2"] = std::make_shared<TMotor>(0x06, "AK70_10", "can2");
+    tmotors["2_R_arm1"] = std::make_shared<TMotor>(0x01, "AK70_10", "can1");
+    tmotors["3_L_arm1"] = std::make_shared<TMotor>(0x02, "AK70_10", "can1");
+    tmotors["4_R_arm2"] = std::make_shared<TMotor>(0x03, "AK70_10", "can1");
 
-    tmotors["5_R_arm3"] = std::make_shared<TMotor>(0x008, "AK70_10", "can1");
-    tmotors["6_L_arm2"] = std::make_shared<TMotor>(0x00A, "AK70_10", "can1");
-    tmotors["7_L_arm3"] = std::make_shared<TMotor>(0x00C, "AK70_10", "can1");
+    tmotors["5_R_arm3"] = std::make_shared<TMotor>(0x004, "AK70_10", "can0");
+    tmotors["6_L_arm2"] = std::make_shared<TMotor>(0x005, "AK70_10", "can0");
+    tmotors["7_L_arm3"] = std::make_shared<TMotor>(0x006, "AK70_10", "can0");
 
     // Canport Initialization
-    std::vector<std::string> ifnames = {"can0","can1", "can2"};
+    std::vector<std::string> ifnames = {"can0","can1"};
     CanSocketUtils canUtils(ifnames);
 
     std::map<std::string, std::shared_ptr<MaxonMotor>> maxonMotors;
