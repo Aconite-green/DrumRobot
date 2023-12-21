e6218a94f29183d256e8b2c23da2b0d2986ff4be
SeongYongPark <pji50260@naver.com>
2023-12-21 10:36:03 +0900

    12_21

--- src/PathManager.cpp
+++ src/PathManager.cpp
@@ -429,7 +429,6 @@ void PathManager::GetReadyArr()
 
     // tmotors의 상태를 확인
     cout << "tmotors size: " << tmotors.size() << "\n";
-    int cnt = 0;
 
     // 각 모터의 현재위치 값 불러오기
     for (auto &entry : tmotors)
@@ -547,8 +546,12 @@ void PathManager::PathLoopTask()
                 Q1[8] = Q1[8] + M_PI / 36;
                 Q2[8] = Q2[8] + M_PI / 36;
             }
-            // 허리는 Q1 ~ Q2 동안 계속 이동
+            // waist & Arm1 & Arm2는 Q1 ~ Q2 동안 계속 이동
             Q1[0] = (Q2[0] + c_MotorAngle[0]) / 2.0;
+            Q1[1] = (Q2[1] + c_MotorAngle[1]) / 2.0;
+            Q1[2] = (Q2[2] + c_MotorAngle[2]) / 2.0;
+            Q1[3] = (Q2[3] + c_MotorAngle[3]) / 2.0;
+            Q1[5] = (Q2[4] + c_MotorAngle[5]) / 2.0;
         }
 
         p_R = c_R;
@@ -626,8 +629,12 @@ void PathManager::PathLoopTask()
             Q3[8] = Q3[8] + M_PI / 36;
             Q4[8] = Q4[8] + M_PI / 36;
         }
-        // 허리는 Q3 ~ Q4 동안 계속 이동
+        // waist & Arm1 & Arm2는 Q3 ~ Q4 동안 계속 이동
         Q3[0] = (Q4[0] + Q2[0]) / 2.0;
+        Q3[1] = (Q4[1] + Q2[1]) / 2.0;
+        Q3[2] = (Q4[2] + Q2[2]) / 2.0;
+        Q3[3] = (Q4[3] + Q2[3]) / 2.0;
+        Q3[5] = (Q4[4] + Q2[5]) / 2.0;
     }
 
     p_R = c_R;
