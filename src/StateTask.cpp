b60c54f695927fbb914f823814f7070a8d826a5d
SeongYongPark <pji50260@naver.com>
2023-12-28 14:23:46 +0900

    12_28

--- src/StateTask.cpp
+++ src/StateTask.cpp
@@ -937,11 +937,11 @@ void StateTask::HomeTMotor(std::shared_ptr<TMotor> &motor, const std::string &mo
     double additionalTorque = 0.0;
     if (motorName == "L_arm2" || motorName == "R_arm2")
     {
-        additionalTorque = motor->cwDir * (-2.2);
+        additionalTorque = motor->cwDir * (-2.5);
     }
     else if (motorName == "L_arm3" || motorName == "R_arm3")
     {
-        additionalTorque = motor->cwDir * 1.0;
+        additionalTorque = motor->cwDir * 1.5;
     }
 
     TParser.parseSendCommand(*motor, &frameToProcess, motor->nodeId, 8, 0, initialDirection, 0, 4.5, additionalTorque);
