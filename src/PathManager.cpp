d1b337c85a33bd2f7a0e376cf2c37da8014f4aae
SeongYongPark <pji50260@naver.com>
2024-02-19 14:18:57 +0900

    2_19

--- src/PathManager.cpp
+++ src/PathManager.cpp
@@ -301,7 +301,8 @@ vector<double> PathManager::IKfun(vector<double> &P1, vector<double> &P2)
         }
     }
 
-    if(first){
+    if (first)
+    {
         std::cout << "IKfun Not Solved!!\n";
         systemState.main = Main::Pause;
     }
@@ -545,16 +546,17 @@ void PathManager::GetMusicSheet()
         else
         {
             vector<int> inst_arr_R(10, 0), inst_arr_L(10, 0);
-            time_arr.push_back(stod(columns[1]) * 100 / bpm);
 
+            if (columns[2] == "0" && columns[3] == "0")
+                continue;
             if (columns[2] != "0")
                 inst_arr_R[instrument_mapping[columns[2]]] = 1;
             if (columns[3] != "0")
                 inst_arr_L[instrument_mapping[columns[3]]] = 1;
 
+            time_arr.push_back(stod(columns[1]) * 100 / bpm);
             RF.push_back(stoi(columns[6]) == 1 ? 1 : 0);
             LF.push_back(stoi(columns[7]) == 2 ? 1 : 0);
-
             RA.push_back(inst_arr_R);
             LA.push_back(inst_arr_L);
         }
