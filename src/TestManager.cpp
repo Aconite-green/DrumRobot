0110caa709512f402a638fca6ca008426f81cc01
SeongYongPark <pji50260@naver.com>
2024-07-15 17:11:13 +0900

    ww

--- src/TestManager.cpp
+++ src/TestManager.cpp
@@ -34,7 +34,7 @@ void TestManager::SendTestProcess()
 
         cout << "\nSelect Method (1 - 관절각도값 조절, 2 - 좌표값 조절, 3 - 단일 회전, 4 - 멀티 회전, 5 - 스틱 타격, 6 - Command , 7 - 나가기, 8 - Break) : ";
         cin >> method;
-
+        //
         if (method == 1)
         {
             state.test = TestSub::SetQValue;
